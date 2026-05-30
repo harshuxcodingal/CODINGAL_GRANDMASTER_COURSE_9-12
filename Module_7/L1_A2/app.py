f10f0d92324fa579c15bccc0d637b97dadbcab87
harshuxcodingal <harshalpetkar09@gmail.com>
2026-05-30 09:06:17 +0530

    Module 7 Comleted

---
+++ Module_7/L1_A2/app.py
@@ -0,0 +1,7 @@
+print("WElcome to Python world of codinggg,g,g,")
+print("This is how we print in python")
+print(4+5)
+
+print("The keywords in python aree:-")
+import keyword
+print(keyword.kwlist)
